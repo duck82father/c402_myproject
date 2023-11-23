69e9073193b472142433eefe59ebfaaf3c517a41
duck82father <legnaevol84@gmail.com>
2023-11-23 09:40:12 +0900

    2023-11-21

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,22 +1,24 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template
+from flask import Blueprint, url_for, request, render_template, g
 from werkzeug.utils import redirect
 
 from pybo import db
 from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
+from pybo.views.auth_views import login_required
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=('POST',))
+@login_required
 def create(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
     if form.validate_on_submit():
         # equest.form['content'] 코드는 POST 폼 방식으로 전송된 데이터 항목 중 name 속성이 content인 값을 의미한다.
         content = request.form['content']
-        answer = Answer(content=content, create_date=datetime.now())
+        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         # question.answer_set은 "질문에 달린 답변들"
         question.answer_set.append(answer)
         db.session.commit()
